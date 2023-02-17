601460f22109f0136eb06bc95482bc843f09b3f0
jeffbengtsson <jeff.bengtsson@gmail.com>
2023-02-17 11:47:47 +0100

    fixed

--- Test/passwordCheck.js
+++ Test/passwordCheck.js
@@ -3,7 +3,9 @@ function checkPassword() {
     var upperCaseLetters = /[A-Z]/g;
     var digits = /[0-9]/g;
 
-    if (password.match(upperCaseLetters) && password.match(upperCaseLetters).length >= 2 && password.match(digits) && password.match(digits).length >= 1 && password.length >= 8)  {
+    if (password.match(upperCaseLetters) && password.match(upperCaseLetters).length >= 2
+        && password.match(digits) && password.match(digits).length >= 1
+        && password.length >= 8)  {
         alert("Valid Password")
         window.location.href = "HomePage.html";
     } else {
